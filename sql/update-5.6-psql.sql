ab303d0da84f20761bed39004161f312a6c07ee5
Gunter Zeilinger <gunterze@gmail.com>
2016-08-02 17:04:49 +0200

    Fix #272 dcm4che/dcm4chee-arc-light : QIDO orderby SeriesNumber or InstanceNumber return literal instead numeric ordered matches

--- sql/update-5.6-psql.sql
+++ sql/update-5.6-psql.sql
@@ -2,10 +2,22 @@ alter table patient add num_studies int4;
 update patient set num_studies = (
   select count(*) from study where study.patient_fk=patient.pk and study.rejection_state in (0,1));
 alter table location add multi_ref int4, add uidmap_fk int8, add object_type int4;
-update location set obj_type = 0;
+update location set object_type = 0;
 alter table location alter object_type set not null;
 alter table location alter tsuid drop not null;
 create table uidmap (pk int8 not null, uidmap bytea not null, primary key (pk));
+
+alter table instance add inst_no_int int4;
+update instance set inst_no_int = inst_no::int4 where inst_no != '*';
+update instance set inst_no_int = null where inst_no = '*';
+alter table instance drop inst_no;
+alter table instance rename inst_no_int to inst_no;
+alter table series add series_no_int int4;
+update series set series_no_int = series_no::int4 where series_no != '*';
+update series set series_no_int = null where series_no = '*';
+alter table series drop series_no;
+alter table series rename series_no_int to series_no;
+
 create index UK_i1lnahmehau3r3j9pdyxg3p3y on location (multi_ref);
 alter table location add constraint FK_bfk5vl6eoxaf0hhwiu3rbgmkn foreign key (uidmap_fk) references uidmap;
 create index FK_bfk5vl6eoxaf0hhwiu3rbgmkn on location (uidmap_fk) ;
