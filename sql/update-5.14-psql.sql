c3e15d3e23a1df218df68710c9d35ea241da05d9
Gunter Zeilinger <gunterze@gmail.com>
2018-11-15 12:18:28 +0100

    provide update-schema bash script in postgres-dcm4chee docker image dcm4che/dcm4chee-arc-light#1677

--- sql/update-5.14-psql.sql
+++ sql/update-5.14-psql.sql
@@ -1,9 +1,6 @@
 -- part 1: can be applied on archive running archive 5.13
 create table stgver_task (pk int8 not null, completed int4 not null, created_time timestamp not null, failed int4 not null, local_aet varchar(255) not null, series_iuid varchar(255), sop_iuid varchar(255), storage_ids varchar(255), stgcmt_policy int4, study_iuid varchar(255) not null, update_location_status boolean, updated_time timestamp not null, queue_msg_fk int8 not null, primary key (pk));
 alter table stgver_task add constraint UK_hch5fanx7ejwew2ag2ividq9r  unique (queue_msg_fk);
-create index UK_fe2td8g77y54d90w7339ka0ix on stgver_task (created_time);
-create index UK_bja5px1r9qts4nydp1a2i61ok on stgver_task (updated_time);
-create index UK_iudr0qmrm15i2evq1733h1ace on stgver_task (study_iuid, series_iuid, sop_iuid);
 alter table stgver_task add constraint FK_hch5fanx7ejwew2ag2ividq9r foreign key (queue_msg_fk) references queue_msg;
 alter table series
   add stgver_time timestamp,
@@ -14,6 +11,11 @@ alter table series
   add compress_failures int4;
 
 update series set stgver_failures = 0, compress_failures = 0;
+
+create index UK_fe2td8g77y54d90w7339ka0ix on stgver_task (created_time);
+create index UK_bja5px1r9qts4nydp1a2i61ok on stgver_task (updated_time);
+create index UK_iudr0qmrm15i2evq1733h1ace on stgver_task (study_iuid, series_iuid, sop_iuid);
+
 create index UK_ftv3ijh2ud6ogoknneyqc6t9i on series (stgver_time);
 create index UK_s1vceb8cu9c45j0q8tbldgol9 on series (stgver_failures);
 create index UK_38mfgfnjhan2yhnwqtcrawe4 on series (compress_time);
@@ -23,7 +25,7 @@ create index FK_hch5fanx7ejwew2ag2ividq9r on stgver_task (queue_msg_fk) ;
 
 create sequence stgver_task_pk_seq;
 
--- part 2: have to be applied while archive is stopped
+-- part 2: shall be applied on stopped archive before starting 5.14
 update series set stgver_failures = 0, compress_failures = 0 where stgver_failures is null;
 
 -- part 3: can be applied on already running archive 5.14
