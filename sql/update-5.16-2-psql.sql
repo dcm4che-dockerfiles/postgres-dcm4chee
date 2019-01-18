834a686a5c053594da8dea073df84206ec21c59c
Gunter Zeilinger <gunterze@gmail.com>
2019-01-18 13:39:30 +0100

    Optionally export expired Studies before rejecting them for Data Retention Expiry dcm4che/dcm4chee-arc-light#1777

--- sql/update-5.16-2-psql.sql
+++ sql/update-5.16-2-psql.sql
@@ -1,1 +1,3 @@
 -- part 2: shall be applied on stopped archive before starting 5.16
+update study set expiration_state = 0 where expiration_state is null;
+update series set expiration_state = 0 where expiration_state is null;
