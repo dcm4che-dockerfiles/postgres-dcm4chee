6cff7058165e966f348277011044e7dd91638fa0
Gunter Zeilinger <gunterze@gmail.com>
2019-04-30 11:22:19 +0200

    Create Retrieve Tasks without scheduling dcm4che/dcm4chee-arc-light#1959

--- sql/update-5.17.1-psql.sql
+++ sql/update-5.17.1-psql.sql
@@ -1,13 +1,21 @@
 -- part 1: can be applied on archive running archive 5.16
 alter table queue_msg add batch_id varchar(255);
-alter table retrieve_task add batch_id varchar(255);
+alter table retrieve_task
+    add device_name varchar(255),
+    add queue_name varchar(255),
+    add batch_id varchar(255);
 alter table retrieve_task alter column queue_msg_fk drop not null;
 alter table stgcmt_result alter column exporter_id drop not null;
 
 update queue_msg set batch_id = batchID;
-update retrieve_task set batch_id = queue_msg.batch_id
-  from queue_msg
-  where queue_msg_fk = queue_msg.pk;
+update retrieve_task
+    set device_name = queue_msg.device_name,
+        queue_name  = queue_msg.queue_name,
+        batch_id    = queue_msg.batch_id
+    from queue_msg
+    where queue_msg_fk = queue_msg.pk;
 
 create index UK_ln9rs61la03lhvgiv8c2wehnr on queue_msg (batch_id);
+create index UK_djkqk3dls3xkru1n0c3p5rm3 on retrieve_task (device_name);
+create index UK_r866eptnxfw7plhxwtm3vks0e on retrieve_task (queue_name);
 create index UK_ahkqwir2di2jm44jlhi22iw3e on retrieve_task (batch_id);
--- sql/update-5.17.3-psql.sql
+++ sql/update-5.17.3-psql.sql
@@ -1,2 +1,5 @@
 -- part 3: can be applied on already running archive 5.17
 alter table queue_msg drop batchID;
+alter table retrieve_task
+    alter device_name set not null,
+    alter queue_name set not null;
