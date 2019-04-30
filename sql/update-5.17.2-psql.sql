3f3510d003be373ac9538ab67b354b6993aa9d28
Gunter Zeilinger <gunterze@gmail.com>
2019-04-30 11:22:19 +0200

    Create Retrieve Tasks without scheduling dcm4che/dcm4chee-arc-light#1959

--- sql/update-5.17.2-psql.sql
+++ sql/update-5.17.2-psql.sql
@@ -1,5 +1,8 @@
 -- part 2: shall be applied on stopped archive before starting 5.17
 update queue_msg set batch_id = batchID where batch_id <> batchID;
-update retrieve_task set batch_id = queue_msg.batch_id
-  from queue_msg
-  where queue_msg_fk = queue_msg.pk and retrieve_task.batch_id <> queue_msg.batch_id;
+update retrieve_task
+    set device_name = queue_msg.device_name,
+        queue_name  = queue_msg.queue_name,
+        batch_id    = queue_msg.batch_id
+    from queue_msg
+    where queue_msg_fk = queue_msg.pk and retrieve_task.device_name is null ;
