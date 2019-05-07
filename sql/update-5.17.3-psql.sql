367dacf349ecf3704500af2bef9982c142eb6346
Gunter Zeilinger <gunterze@gmail.com>
2019-05-07 15:39:25 +0200

    dcm4che/dcm4chee-arc-light#1971 dcm4che/dcm4chee-arc-light#1972

--- sql/update-5.17.3-psql.sql
+++ sql/update-5.17.3-psql.sql
@@ -3,3 +3,5 @@ alter table queue_msg drop batchID;
 alter table retrieve_task
     alter device_name set not null,
     alter queue_name set not null;
+alter table series alter metadata_update_failures set not null;
+alter table metadata alter created_time set not null;
