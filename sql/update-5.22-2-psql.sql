6cb747193a05053fe00cebcdfbc136ab96af7921
Gunter Zeilinger <gunterze@protonmail.com>
2020-02-27 11:46:53 +0100

    Separate MWL by Archive AE dcm4che/dcm4chee-arc-light#2467

--- sql/update-5.22-2-psql.sql
+++ sql/update-5.22-2-psql.sql
@@ -1,3 +1,3 @@
 -- part 2: shall be applied on stopped archive before starting 5.22
-update mwl_item set admission_id = '*', institution = '*', department = '*' where admission_id is null;
+update mwl_item set local_aet = '*', admission_id = '*', institution = '*', department = '*' where local_aet is null;
 update study set admission_id = '*' where admission_id is null;
--- sql/update-5.22-3-psql.sql
+++ sql/update-5.22-3-psql.sql
@@ -1,5 +1,6 @@
 -- part 3: can be applied on already running archive 5.22
 alter table mwl_item
+    alter local_aet set not null,
     alter admission_id set not null,
     alter institution set not null,
     alter department set not null;
