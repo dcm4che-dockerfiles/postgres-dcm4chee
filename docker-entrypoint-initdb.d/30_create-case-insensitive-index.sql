af6beb017f59b813a5f752c9db4ee7fba8d05da3
Gunter Zeilinger <gunterze@protonmail.com>
2020-11-13 12:31:00 +0100

    Make literal person name matching fully DICOM compliant dcm4che/dcm4chee-arc-light#2837

--- docker-entrypoint-initdb.d/30_create-case-insensitive-index.sql
+++ docker-entrypoint-initdb.d/30_create-case-insensitive-index.sql
@@ -1,3 +1,7 @@
+create index alphabetic_name_upper_idx on person_name (upper(alphabetic_name));
+create index ideographic_name_upper_idx on person_name (upper(ideographic_name));
+create index phonetic_name_upper_idx on person_name (upper(phonetic_name));
+
 create index family_name_upper_idx on person_name (upper(family_name));
 create index given_name_upper_idx on person_name (upper(given_name));
 create index middle_name_upper_idx on person_name (upper(middle_name));
