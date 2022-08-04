544a82f29993273abd3a88ceed19d1d4980a3626
Gunter Zeilinger <gunterze@protonmail.com>
2022-08-04 13:16:29 +0200

    RESTful service to set, get, delete a value associated to a specified key dcm4che/dcm4chee-arc-light#3748

---
+++ sql/update-5.28-1-psql.sql
@@ -0,0 +1,20 @@
+-- can be applied on archive running archive 5.27
+create table key_value
+(
+    pk           int8          not null,
+    content_type varchar(255)  not null,
+    created_time timestamp     not null,
+    key          varchar(255)  not null,
+    updated_time timestamp     not null,
+    username     varchar(255),
+    value        varchar(4000) not null,
+    primary key (pk)
+);
+
+alter table key_value
+    add constraint UK_gvyp924rq0a5y8u3emjs35g2q unique (key);
+
+create index UK_o92kvbnsf2cnttpubgfpd6p01 on key_value (username);
+create index UK_51ia14mc4pabswlvaqnt43clb on key_value (updated_time);
+
+create sequence key_value_pk_seq;
