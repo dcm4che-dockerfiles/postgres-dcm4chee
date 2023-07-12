a0d22771e6f0211c3bf66a523ee704b996971177
Gunter Zeilinger <gunterze@protonmail.com>
2023-07-12 15:06:48 +0200

    update-5.26-<db>.sql: several SQL statements are wrongly listed in part 2 instead of in part 1 dcm4che/dcm4chee-arc-light#4130

--- sql/update-5.26-2-psql.sql
+++ sql/update-5.26-2-psql.sql
@@ -1,22 +1,4 @@
 -- part 2: shall be applied on stopped archive before starting 5.26
-update mpps
-set (accno_entity_id, accno_entity_uid, accno_entity_uid_type) =
-        (select issuer.entity_id, issuer.entity_uid, issuer.entity_uid_type
-         from issuer where accno_issuer_fk = issuer.pk)
-where accno_issuer_fk is not null;
-
-update mwl_item
-set (accno_entity_id, accno_entity_uid, accno_entity_uid_type) =
-        (select issuer.entity_id, issuer.entity_uid, issuer.entity_uid_type
-         from issuer where accno_issuer_fk = issuer.pk)
-where accno_issuer_fk is not null;
-
-update mwl_item
-set (admid_entity_id, admid_entity_uid, admid_entity_uid_type) =
-        (select issuer.entity_id, issuer.entity_uid, issuer.entity_uid_type
-         from issuer where admid_issuer_fk = issuer.pk)
-where admid_issuer_fk is not null;
-
 update patient_id
 set (entity_id, entity_uid, entity_uid_type) =
         (select issuer.entity_id, issuer.entity_uid, issuer.entity_uid_type
@@ -24,36 +6,6 @@ set (entity_id, entity_uid, entity_uid_type) =
 where issuer_fk is not null
   and entity_id is null
   and entity_uid is null;
-
-update series_req
-set (accno_entity_id, accno_entity_uid, accno_entity_uid_type) =
-        (select issuer.entity_id, issuer.entity_uid, issuer.entity_uid_type
-         from issuer where accno_issuer_fk = issuer.pk)
-where accno_issuer_fk is not null;
-
-update study
-set (accno_entity_id, accno_entity_uid, accno_entity_uid_type) =
-        (select issuer.entity_id, issuer.entity_uid, issuer.entity_uid_type
-         from issuer where accno_issuer_fk = issuer.pk)
-where accno_issuer_fk is not null;
-
-update study
-set (admid_entity_id, admid_entity_uid, admid_entity_uid_type) =
-        (select issuer.entity_id, issuer.entity_uid, issuer.entity_uid_type
-         from issuer where admid_issuer_fk = issuer.pk)
-where admid_issuer_fk is not null;
-
-update ups
-set (admid_entity_id, admid_entity_uid, admid_entity_uid_type) =
-        (select issuer.entity_id, issuer.entity_uid, issuer.entity_uid_type
-         from issuer where ups.admission_issuer_fk = issuer.pk)
-where admission_issuer_fk is not null;
-
-update ups_req
-set (accno_entity_id, accno_entity_uid, accno_entity_uid_type) =
-        (select issuer.entity_id, issuer.entity_uid, issuer.entity_uid_type
-         from issuer where accno_issuer_fk = issuer.pk)
-where accno_issuer_fk is not null;
 
 insert into rel_ups_station_name_code
 select ups.pk, ups.station_name_fk from ups where ups.station_name_fk is not null;
