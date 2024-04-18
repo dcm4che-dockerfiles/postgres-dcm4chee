2bc3ee7045222060a710d3264dca905f06d52b44
Gunter Zeilinger <gunterze@protonmail.com>
2024-04-18 17:02:54 +0200

    Reject concurrent requests to permanently delete a study dcm4che/dcm4chee-arc-light#4462

--- sql/update-5.32-3-psql.sql
+++ sql/update-5.32-3-psql.sql
@@ -1,4 +1,4 @@
 -- part 3: can be applied on already running archive 5.32
 alter table instance alter ext_retrieve_aet set not null;
 alter table series alter ext_retrieve_aet set not null;
-alter table study alter deleting set not null;
+alter table study alter study_deleting set not null;
