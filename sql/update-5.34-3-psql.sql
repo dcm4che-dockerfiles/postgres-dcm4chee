7d6c911e1b19602c0298c8d66fbfbff16014fdd1
gunterze <gunterze@protonmail.com>
2025-04-15 10:57:34 +0200

    Prevent duplicate Patient (ID) records by DB Unique Constraint dcm4che/dcm4chee-arc-light#4636

--- sql/update-5.34-3-psql.sql
+++ sql/update-5.34-3-psql.sql
@@ -1,5 +1,3 @@
 -- part 3: can be applied on already running archive 5.34
 alter table patient_id
-    alter entity_id set not null,
-    alter entity_uid set not null,
-    alter entity_uid_type set not null;
+    alter pat_name set not null;
