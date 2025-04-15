cfac18b18347ee161a3ef2048e2d93e94fb5768c
gunterze <gunterze@protonmail.com>
2025-04-15 10:57:34 +0200

    Prevent duplicate Patient (ID) records by DB Unique Constraint dcm4che/dcm4chee-arc-light#4636

--- sql/update-5.34-2-psql.sql
+++ sql/update-5.34-2-psql.sql
@@ -1,9 +1,10 @@
 -- part 2: shall be applied on stopped archive before starting 5.34
-update patient_id set entity_id = entity_uid
-    where entity_id is null and entity_uid is not null;
-update patient_id set entity_uid = concat('iss:', entity_id), entity_uid_type = 'URI'
-    where entity_uid is null and entity_id is not null;
-update patient_id set entity_id = '*', entity_uid = 'iss:*', entity_uid_type = 'URI'
-    where entity_id is null;
-alter table patient_id add constraint patient_id_pat_id_entity_id_entity_uid_entity_uid_type_key
-    unique (pat_id, entity_id, entity_uid, entity_uid_type);
+update patient_id set pat_name = (
+    select person_name.alphabetic_name from person_name
+        join patient on person_name.pk = patient.pat_name_fk
+        where patient.pk = patient_id.patient_fk)
+    where pat_name is null;
+update patient_id set pat_name = '*'
+    where pat_name is null;
+alter table patient_id add constraint patient_id_pat_id_pat_name_key
+    unique (pat_id, pat_name);
