8d1f4954cd8212a7082f8c909bb2baebdf206601
gunterze <gunterze@protonmail.com>
2025-04-18 11:06:19 +0200

    Prevent duplicate Patient (ID) records by DB Unique Constraint dcm4che/dcm4chee-arc-light#4636

--- sql/update-5.34-1-psql.sql
+++ sql/update-5.34-1-psql.sql
@@ -5,7 +5,11 @@ create index FKhy3cd5se2avt08upapu19y1g6 on ups (perf_name_fk) ;
 
 alter table patient_id add pat_name varchar(255);
 update patient_id set pat_name = (
-    select person_name.alphabetic_name from person_name
+    select rtrim(concat(
+        rtrim(person_name.alphabetic_name, '^'), '=',
+        rtrim(person_name.ideographic_name, '^'), '=',
+        rtrim(person_name.phonetic_name, '^')), '=')
+    from person_name
         join patient on person_name.pk = patient.pat_name_fk
         where patient.pk = patient_id.patient_fk );
 update patient_id set pat_name = '*'
