7ee359d6da785de0a69ae5e49d9df943f22acabe
gunterze <gunterze@protonmail.com>
2025-04-18 11:06:19 +0200

    Prevent duplicate Patient (ID) records by DB Unique Constraint dcm4che/dcm4chee-arc-light#4636

--- sql/update-5.34-2-psql.sql
+++ sql/update-5.34-2-psql.sql
@@ -1,6 +1,10 @@
 -- part 2: shall be applied on stopped archive before starting 5.34
 update patient_id set pat_name = (
-    select person_name.alphabetic_name from person_name
+    select rtrim(concat(
+        rtrim(person_name.alphabetic_name, '^'), '=',
+        rtrim(person_name.ideographic_name, '^'), '=',
+        rtrim(person_name.phonetic_name, '^')), '=')
+    from person_name
         join patient on person_name.pk = patient.pat_name_fk
         where patient.pk = patient_id.patient_fk)
     where pat_name is null;
