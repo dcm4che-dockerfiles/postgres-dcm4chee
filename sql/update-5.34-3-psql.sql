32243bc6b4d6b851c74950ec6c3d34da31b197aa
Jenkins <jenkins@j4care.com>
2025-04-30 16:33:18 +0200

    Avoid loading DICOM objects form storage on updating exisiting aggregated Series Metadata on attributes update dcm4che/dcm4chee-arc-light#4713

--- sql/update-5.34-3-psql.sql
+++ sql/update-5.34-3-psql.sql
@@ -1,3 +1,6 @@
 -- part 3: can be applied on already running archive 5.34
 alter table patient_id
     alter pat_name set not null;
+
+alter table series
+    alter metadata_update_load_objects set not null;
